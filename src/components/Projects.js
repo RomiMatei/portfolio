2422164d266a33c42d23b2384e7170cf9901c360
Romina Matei <romi3783@gmail.com>
2023-04-06 09:46:17 +0200

    🐛 PROJECTS > Fix typo

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,20 +4,19 @@ export default function Projects({ name, id, imageSource, languages }) {
   return (
     <ProjectsStyled>
       <div className="projects">
+        <div className="project-info">
+          <h3 className="project-name">{name}</h3>
+          <div className="project-languages">
+            {languages.map((language) => {
+              return <span key={id}>{language}</span>;
+            })}
+          </div>
+        </div>
         <div className="project" key={id}>
           <div className="project-image">
             <img src={imageSource} alt="" />
           </div>
-          <div className="project-info">
-            <h3 className="project-name">{name}</h3>
-            <div className="project-languages">
-              {languages.map((language) => {
-                return <span key={id}>{language}</span>;
-              })}
-            </div>
-          </div>
         </div>
-        );
       </div>
     </ProjectsStyled>
   );
